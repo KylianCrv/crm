4f86e3e251e23006b98c2939c04e67b73a64c000
KylianCrv <kylian.crv@hotmail.com>
2022-09-21 11:40:50 +0200

    Commit 2 import module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { UiModule } from '../ui/ui.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [IconsModule, LoginModule, UiModule, TemplatesModule],
 })
-export class CoreModule { }
+export class CoreModule {}
